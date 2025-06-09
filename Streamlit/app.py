48cb8730b6329997ba06eb59af049016e7f1e108
Nico-Draagron <nicolas.draagron@gmail.com>
2025-06-09 09:06:12 -0300

    Create app.py

---
+++ Streamlit/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Pesquisa AMF", layout="wide")
+st.title("🔍 Projeto Pesquisa AMF")
+st.markdown("Bem-vindo ao painel principal. Use o menu lateral para navegar entre as páginas.")
